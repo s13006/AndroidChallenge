b072c3b9368fb6963d433c6850f4609b4d01431d
s13006 <s13006@std.it-college.ac.jp>
2014-08-27 21:28:04 +0900

    課題提出

--- app/src/main/java/jp/ac/it_college/std/s13006/androidchallenge/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13006/androidchallenge/MainActivity.java
@@ -1,44 +1,39 @@
 package jp.ac.it_college.std.s13006.androidchallenge;
 
 import android.app.Activity;
-import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+    TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
@@ -46,10 +41,7 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment{
 
         public PlaceholderFragment() {
         }
@@ -59,6 +51,51 @@ public class MainActivity extends Activity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
+
         }
+
     }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK) {
+            new AlertDialog.Builder(this)
+                    .setTitle("終了の確認")
+                    .setMessage("tetrisを終了しますか？")
+                    .setPositiveButton("はい",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            }
+                    )
+                    .setNegativeButton("キャンセル",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                }
+                            }
+                    )
+                    .show();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if(textView == null) {
+            difficulty fragment = difficulty.newInstance();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.start_fragment, fragment);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            ft.commit();
+
+            textView = (TextView) findViewById(R.id.touch_start);
+            textView.setVisibility(View.INVISIBLE);
+        }
+        return true;
+    }
+
 }
